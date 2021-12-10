d955e9bc4debb1d22855e8b7c72162f5f7410214
Nadejda Doborina <NadejdaFort@gmail.com>
2021-12-10 17:04:21 +0300

    lesson6

---
+++ src/nfort/com/oop/lesson1/Computer.java
@@ -0,0 +1,4 @@
+package nfort.com.oop.lesson1;
+
+public class Computer {
+}
---
+++ src/nfort/com/oop/lesson6/Lesson6Runner.java
@@ -0,0 +1,12 @@
+package nfort.com.oop.lesson6;
+
+import nfort.com.oop.lesson1.Computer;
+
+public class Lesson6Runner {
+    public static void main(String[] args) {
+        Integer integer;    // java.lang подключен по-умолчанию ко всем пакетам
+
+        nfort.com.oop.lesson2.Computer computer = new nfort.com.oop.lesson2.Computer();
+        Computer computer1 = new Computer();
+    }
+}
