358571f9b08b247dc24ffa6f9f9004630ab1dec3
Nadejda Doborina <NadejdaFort@gmail.com>
2021-12-12 20:51:14 +0300

    lesson12_3

--- src/nfort/com/oop/lesson11/Laptop.java
+++ src/nfort/com/oop/lesson11/Laptop.java
@@ -6,8 +6,20 @@ package nfort.com.oop.lesson11;
 public class Laptop extends Computer{
     private int weight;
 
+    {
+        System.out.println("init block laptop");
+    }
+
+    static {
+        System.out.println("static init bloc laptop");
+    }
+
     public Laptop(Ssd ssd, Ram ram, int weight){
         this.weight = weight;
+    }
+
+    public Laptop() {
+        System.out.println("constructor laptop");
     }
 
     public void open() {
