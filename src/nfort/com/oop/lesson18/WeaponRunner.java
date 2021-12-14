7793558470bfdbcb27da70877fd760bdb4d2b3dc
Nadejda Doborina <NadejdaFort@gmail.com>
2021-12-14 14:05:54 +0300

    lesson18_3

--- src/nfort/com/oop/lesson18/WeaponRunner.java
+++ src/nfort/com/oop/lesson18/WeaponRunner.java
@@ -1,11 +1,13 @@
 package nfort.com.oop.lesson18;
 
 import nfort.com.oop.lesson15.Archer;
+import nfort.com.oop.lesson15.Hero;
 import nfort.com.oop.lesson15.Mage;
 import nfort.com.oop.lesson15.Warrior;
 import nfort.com.oop.lesson18.weapon.Bow;
 import nfort.com.oop.lesson18.weapon.Sword;
 import nfort.com.oop.lesson18.weapon.Wand;
+import nfort.com.oop.lesson18.weapon.Weapon;
 
 public class WeaponRunner {
 
@@ -19,5 +21,15 @@ public class WeaponRunner {
 
         Mage<Wand> mage = new Mage<>("Гендольф", 20);
         mage.setWeapon(new Wand());
+
+        printWeaponDamage(archer);
     }
+
+    public static <T extends Weapon> void printWeaponDamage(Hero<T> hero) {
+        System.out.println(hero.getWeapon().getDamage());
+    }
+
+//    public static void printWeaponDamage(Hero<? extends Weapon> hero) {
+//        System.out.println(hero.getWeapon().getDamage());
+//    }
 }
