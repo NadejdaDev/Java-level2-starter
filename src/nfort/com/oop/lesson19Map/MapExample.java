306c511f8af2d6d69c58251518cafd5e053b4353
Nadejda Doborina <NadejdaFort@gmail.com>
2021-12-17 02:33:00 +0300

    lesson19_9

--- src/nfort/com/oop/lesson19Map/MapExample.java
+++ src/nfort/com/oop/lesson19Map/MapExample.java
@@ -16,11 +16,31 @@ public class MapExample {
         map.put(ivan.getId(), ivan);
         map.put(petr.getId(), petr);
         map.put(null, new Person(3, "", ""));
-        map.put(null, new Person(4, "", ""));
+        map.putIfAbsent(null, new Person(4, "", ""));
 
+        System.out.println(map.keySet());
+        System.out.println(map.values());
+        System.out.println(map.entrySet());
 
-        System.out.println(map.get(2));
-        System.out.println(map.get(null));
+        for(Person person : map.values()) {
+            System.out.println(person.getFirstName());
+        }
+
+        for(Integer key : map.keySet()) {
+            System.out.println(key);
+        }
+
+        for(Map.Entry<Integer, Person> entry : map.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue() + ", Class: " + entry.getClass());
+        }
+
+        System.out.println(map.containsKey(2));
+        System.out.println(map.size());
+        System.out.println(map.isEmpty());
+        System.out.println(map.getOrDefault(6, new Person(4, "Sveta", "Svetikova")));
+
+//        System.out.println(map.get(2));
+//        System.out.println(map.get(null));
 
     }
 }
