92015e20afe9a95e126d838db6cd209e218231bd
Nadejda Doborina <NadejdaFort@gmail.com>
2021-12-20 02:55:05 +0300

    lesson21_3

--- src/nfort/com/oop/lesson21/StreamExample.java
+++ src/nfort/com/oop/lesson21/StreamExample.java
@@ -1,20 +1,53 @@
 package nfort.com.oop.lesson21;
 
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.function.IntUnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamExample {
 
     public static void main(String[] args) {
         List<String> strings = List.of("88", "11", "22", "33", "44", "55", "66", "77");
 
-        strings.stream()
+        IntSummaryStatistics intSummaryStatistics = strings.stream()
                 .map(string -> string + string)
                 .map(Integer::valueOf)
                 .filter(value -> value % 2 == 0)
                 .sorted()
                 .skip(1)
                 .limit(2)
+                .mapToInt(Integer::intValue)
+//                .mapToObj(Integer::valueOf)
+                .summaryStatistics();
+
+        System.out.println(intSummaryStatistics);
+
+        List<String> collect = Stream.of("88", "11", "22", "33", "44", "55", "66", "77")
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+
+        IntStream.of(1, 2, 3, 4, 5, 6, 7);
+
+        IntStream.range(0, 10)
+                .forEach(System.out :: println);
+
+//        IntStream.iterate(0, new IntUnaryOperator() {
+//            @Override
+//            public int applyAsInt(int operand) {
+//                return operand + 3;
+//            }
+//        })
+        IntStream.iterate(0, operand -> operand + 3)
+                .skip(10)
+                .limit(20)
                 .forEach(System.out::println);
+
+//        for(int i = 0; i < 10; i++) {
+//            System.out.println(i);
+//        }
 
 //        for(String string : strings) {
 //            String value = string + string;
