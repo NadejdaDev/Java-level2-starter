a9b383c4306b12ce5225e3ecc946bac897f0c179
Nadejda Doborina <NadejdaFort@gmail.com>
2021-12-21 02:12:04 +0300

    lesson21_5

--- src/nfort/com/oop/lesson21/MapReduceExample.java
+++ src/nfort/com/oop/lesson21/MapReduceExample.java
@@ -1,5 +1,6 @@
 package nfort.com.oop.lesson21;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
@@ -12,7 +13,7 @@ import java.util.stream.Stream;
 public class MapReduceExample {
 
     public static void main(String[] args) {
-        Stream.of(
+        Optional<Student> maybeStudent = Stream.of(
                 new Student(18, "Ivan"),
                 new Student(23, "Peter"),
                 new Student(38, "Sveta"),
@@ -22,12 +23,18 @@ public class MapReduceExample {
                 new Student(17, "Lisa"),
                 new Student(23, "Bob")
         )
-//                .sequential()           // обработка в одном потоке
-                .parallel()             // обработка в нескольких потоках
-                .map(Student::getAge)
-//                .reduce(Math::max)
-                .reduce(Math::min)
-//                .reduce(Integer::sum)
+                .sequential()           // обработка в одном потоке
+//                .flatMap(student -> student.getMarks().stream())          // преобразуем в stream<Integer>
+//                .map(mark -> )
+//                .map(Student::getAge)
+//                .filter(student -> student.getAge() < 17)
+                .reduce((student1, student2) -> student1.getAge() > student2.getAge() ? student1 : student2);   // находим максимального по возрасту студента
+
+        maybeStudent.ifPresent(System.out::println);
+
+        maybeStudent.map(Student::getAge)
+//                .filter(age -> age > 25)
+                .flatMap(age -> Optional.of(age * 2))
                 .ifPresent(System.out::println);
     }
 }
--- src/nfort/com/oop/lesson21/Student.java
+++ src/nfort/com/oop/lesson21/Student.java
@@ -1,8 +1,12 @@
 package nfort.com.oop.lesson21;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     private String name;
     private int age;
+    private List<Integer> marks = new ArrayList<>();
 
     public Student (int age, String name){
         this.name = name;
@@ -15,5 +19,17 @@ public class Student {
 
     public int getAge() {
         return age;
+    }
+
+    public List<Integer> getMarks() {
+        return marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
