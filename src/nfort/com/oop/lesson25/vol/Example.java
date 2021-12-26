9f4e263f04aaa4a238c1853646cad47a90a83d8b
Nadejda Doborina <NadejdaFort@gmail.com>
2021-12-27 02:48:31 +0300

    lesson25_8

---
+++ src/nfort/com/oop/lesson25/vol/Example.java
@@ -0,0 +1,18 @@
+package nfort.com.oop.lesson25.vol;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example {
+
+    private List<Integer> list;
+
+    public Example(List<Integer> list) {
+//        this.list = List.copyOf(list);
+        this.list = list;
+    }
+
+    public List<Integer> getList() {
+        return new ArrayList<>(list);
+    }
+}
