2489c3daecc96084258737f478a181a0864305b6
Nadejda Doborina <NadejdaFort@gmail.com>
2021-12-26 17:04:38 +0300

    lesson25_3

--- src/nfort/com/oop/lesson25/ThreadDemo.java
+++ src/nfort/com/oop/lesson25/ThreadDemo.java
@@ -5,16 +5,19 @@ public class ThreadDemo {
     public static void main(String[] args) {
 
         var simpleThread = new SimpleThread();     // 3 способа создать поток
+        System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
         var runnableThread = new Thread(new SimpleRunnable());
         var lambdaThread = new Thread(() -> System.out.println("Hello from lambda " + Thread.currentThread().getName()));
 
 //        simpleThread.run();                                 // поток main просто вызывает функцию run()
         simpleThread.start();                               // java-приложение создает новый поток, и в этом новом потоке вызывается run()
+        System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
         runnableThread.start();
         lambdaThread.start();
 
         try {
-            simpleThread.join();
+            simpleThread.join(100L);            // если SimpleThread не успеет выполниться за 100 миллисекунд - поток main все-равно продолжит выполнение дальше
+            System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
             runnableThread.join();
             lambdaThread.join();
         } catch (InterruptedException e) {
