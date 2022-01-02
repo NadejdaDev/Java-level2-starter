4f3bfbd628fbbab9c51ae0eb839c5452736fce1a
Nadejda Doborina <NadejdaFort@gmail.com>
2022-01-02 21:48:28 +0300

    lesson25_12

---
+++ src/nfort/com/oop/lesson25/task/ProducerThread.java
@@ -0,0 +1,36 @@
+package nfort.com.oop.lesson25.task;
+
+import java.util.Queue;
+
+public class ProducerThread implements Runnable {
+
+    private final Queue<Integer> list;
+
+    public ProducerThread(Queue<Integer> list) {
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        synchronized (list) {
+            while (true) {
+                if (list.size() < 10) {
+                    int random = RandomUtil.getRandom();
+                    list.add(random);
+                    System.out.println("producer adds value: " + random + ". Size: " + list.size());
+                } else {
+                    System.out.println("producer does nothing");
+                }
+//                list.notifyAll();
+                try {
+//                    int randomWaitValue = RandomUtil.getRandom();
+                    int randomWaitValue = 20;
+                    System.out.println("producer waits: " + randomWaitValue);
+                    list.wait(randomWaitValue);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
