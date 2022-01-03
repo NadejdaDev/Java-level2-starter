53c14ee49a1a426823e843d18623f0889b786793
Nadejda Doborina <NadejdaFort@gmail.com>
2022-01-04 00:25:01 +0300

    lesson26_5

--- src/nfort/com/oop/lesson26/pool/ThreadPoolDemo.java
+++ src/nfort/com/oop/lesson26/pool/ThreadPoolDemo.java
@@ -1,15 +1,36 @@
 package nfort.com.oop.lesson26.pool;
 
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class ThreadPoolDemo {
 
-    public static void main(String[] args) {
-        Executors.newSingleThreadExecutor();
-        Executors.newFixedThreadPool(5);
-        Executors.newCachedThreadPool();
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+//        Executors.newSingleThreadExecutor();
+//        Executors.newFixedThreadPool(5);
+//        Executors.newCachedThreadPool();
+//
+//        Executors.newScheduledThreadPool(3);
+//        Executors.newWorkStealingPool();
 
-        Executors.newScheduledThreadPool(3);
-        Executors.newWorkStealingPool();
+        ScheduledExecutorService threadPool = Executors.newScheduledThreadPool(4);
+//        threadPool.schedule(() -> System.out.println("OK!"), 3L, TimeUnit.SECONDS);
+//        threadPool.shutdown();
+//        threadPool.awaitTermination(1L, TimeUnit.HOURS);
+
+        threadPool.scheduleAtFixedRate(() -> System.out.println("OK!"), 2L, 4L, TimeUnit.SECONDS);
+    }
+
+    private static void test() throws InterruptedException, ExecutionException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+        Future<Integer> future = threadPool.submit(() -> {
+            Thread.sleep(2000L);
+            System.out.println("It's callable");
+            return 1;
+        });
+
+        System.out.println("Result: " + future.get());
+        threadPool.shutdown();
+        threadPool.awaitTermination(1L, TimeUnit.HOURS);  // ожидание в течение часа
+        System.out.println("main end");
     }
 }
