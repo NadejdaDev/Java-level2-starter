4a2892911ec13aad86cca65cff795f050b8ce1a7
Nadejda Doborina <NadejdaFort@gmail.com>
2022-01-05 00:03:50 +0300

    lesson26_7

---
+++ src/nfort/com/oop/lesson26/latch/LatchDemo.java
@@ -0,0 +1,24 @@
+package nfort.com.oop.lesson26.latch;
+
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class LatchDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(RocketDetail.values().length);
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        executorService.submit(new Rocket(countDownLatch));     // один из трех потоков сразу займет поток Rocket
+
+        Arrays.stream(RocketDetail.values())
+                .map(detail -> new RocketDetailRunnable(detail, countDownLatch))
+                .forEach(executorService::submit);
+
+        executorService.shutdown();
+        executorService.awaitTermination(1L, TimeUnit.MINUTES);
+    }
+}
