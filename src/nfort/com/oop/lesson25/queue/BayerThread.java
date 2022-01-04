8ccd22bf6a63115c447b141075a7cc232732fb1b
Nadejda Doborina <NadejdaFort@gmail.com>
2022-01-05 01:58:39 +0300

    lesson26_9

--- src/nfort/com/oop/lesson25/queue/BayerThread.java
+++ src/nfort/com/oop/lesson25/queue/BayerThread.java
@@ -2,23 +2,26 @@ package nfort.com.oop.lesson25.queue;
 
 import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Semaphore;
 
 public class BayerThread implements Runnable {
 
-    private final BlockingQueue<Cashbox> cashboxes;
+    private final Semaphore cashboxes;
 
-    public BayerThread(BlockingQueue<Cashbox> cashboxes) {
+    public BayerThread(Semaphore cashboxes) {
         this.cashboxes = cashboxes;
     }
 
     @Override
     public void run() {
         try {
-            Cashbox cashbox = cashboxes.take();    //take делает synchronized и wait вместе
-            System.out.println(Thread.currentThread().getName() + " обслуживается вкассе " + cashbox);
+            cashboxes.acquire();    //получить монитор, если это получилось, то поток идет дальше выполнять инструкции, если нет, то ждет здесь
+
+            System.out.println(Thread.currentThread().getName() + " обслуживается в какой-то кассе");
             Thread.sleep(5L);
-            System.out.println(Thread.currentThread().getName() + " освобождаю кассу " + cashbox);
-            cashboxes.add(cashbox);                 // можно также использовать put()
+            System.out.println(Thread.currentThread().getName() + " освобождаю какую-то кассу");
+
+            cashboxes.release();             // освобождение монитора
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
